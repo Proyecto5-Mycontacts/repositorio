1a6ca666a2f90fa6444112ae625e846f44f6baba
Eric Petit <9199.joan23@fje.edu>
2017-01-31 18:17:21 +0100

    main

--- main.php
+++ main.php
@@ -74,6 +74,9 @@
                         <a href="#page-top"></a>
                     </li>
                     <li>
+                        <a href="anadir_contacto.php" >Añadir contacto</a>
+                    </li>
+                    <li>
                         <a href="modificar_usuario.php" >Edita tu perfil</a>
                     </li>
                     <li>

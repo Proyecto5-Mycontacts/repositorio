962fd7c8a6a57ef03871c7c8213b9631c0a6bf91
Eric Petit <9199.joan23@fje.edu>
2017-02-01 14:04:10 +0100

    Cambios bbdd

--- bd_mycontacts.sql
+++ bd_mycontacts.sql
@@ -49,6 +49,19 @@ INSERT INTO `tbl_contacto` (`cont_id`, `cont_nombre`, `cont_apellido`, `cont_ema
 (1, 'marc', 'petit', 'mpetit92@gmail.com', 636388745, 936573788, 'img/mpetit.png', 'plaça de les palmeres num. 2 ', 'av. mare de deu belvitge', '1992-09-16', 'Esto es una nota', 2),
 (2, 'Eric', 'Petit', 'eric@gmail.com', 67628655, 936373888, 'img/epetit', 'plaça de les palmeres num. 2', 'av. mare de deu belvitge', '1994-02-18', 'hola!!!', 1);
 
+INSERT INTO `tbl_contacto` (`cont_nombre`,`cont_apellido`,`cont_email`,`cont_telefono1`,`cont_telefono2`,`cont_foto`,`cont_direccion1`,`cont_direccion2`,`cont_cumpleaños`,`usu_id`) 
+VALUES 
+("Thaddeus","Stuart","Mauris.vel@eusem.ca","804635263","572635065","7","Torrent de Olla, 219 08012 Barcelona","Blind River","1994-08-11",2),
+("Brock","Howe","sagittis.semper@placerategetvenenatis.net","305717080","227174950","5","Passeig de Gràcia, 48 08007 Barcelona","Verrebroek","1994-08-04",4),
+("Herrod","Hines","non@natoquepenatibuset.edu","738636980","745112807","9"," C/Les Glories Esq. C/ Llacunna, 155","Chartres","1994-12-12",2),
+("Channing","Spence","mauris@facilisiSed.net","433104586","130983953","19","Paseo Potosí, 2","Vancouver","1994-12-04",1),
+("Quintessa","Maynard","urna@ullamcorperviverraMaecenas.co.uk","265804966","653348226","47","Ctra. Nacional II, Km. 588","Cabo de Hornos","1994-06-10",6),
+("Leroy","Rich","diam@cursusnonegestas.net","842570114","355681938","41","Ctra. Manresa A Berga, Km. 1","Price","1994-12-10",8),
+("Gil","Church","ac.urna@dis.org","849542038","170610958","47","Avda. Barcelona, 41-49","Giustino","1994-03-02",5),
+("Ulysses","Harvey","sociis.natoque.penatibus@urnanecluctus.ca","948365494","549806050","13","C/ La Llotja, 6","Andacollo","1994-04-07",1),
+("Tarik","Norman","Mauris@egetdictumplacerat.co.uk","066404230","913847291","33","C/ CONSELL DE CENT, 366","Hijuelas","1994-08-11",5),
+("Nora","Bowman","montes.nascetur.ridiculus@sagittissemper.edu","250855678","633986791","25","C/ MALLORCA, 135","Schulen","1994-07-06",9);
+
 -- --------------------------------------------------------
 
 --
@@ -70,7 +83,17 @@ CREATE TABLE `tbl_usuario` (
 INSERT INTO `tbl_usuario` (`usu_id`, `usu_nombre`, `usu_password`, `usu_email`, `usu_foto`) VALUES
 (1, 'mpetit', '12345', 'mpetit92@gmail.com', 'img/mpetit.png'),
 (2, 'epetit', '12345', 'epetit@gmail.com', 'img/epetit.png'),
-(3, 'admin', 'admin', 'admin@email.com', 'img/admin.png');
+
+
+INSERT INTO `tbl_usuario` (`usu_nombre`,`usu_password`,`usu_email`,`usu_foto`) 
+VALUES ("Dieter","7979","Phasellus.nulla@elementumategestas.ca","4.jpg"),
+("Palmer","3106","sed@velfaucibusid.net","6.jpg"),
+("Quamar","2731","eu.lacus.Quisque@ullamcorper.edu","3.jpg"),
+("Mannix","9750","dignissim@facilisisvitae.com","4.jpg"),
+("Macon","8381","non.cursus@eros.ca","6.jpg"),
+("Silas","6890","bibendum@risusatfringilla.org","1.jpg"),
+("Nicholas","6554","luctus@eget.com","9.jpg"),
+("Macon","5080","tellus.Aenean.egestas@rutrum.co.uk","6.jpg"),
 
 --
 -- Índices para tablas volcadas
